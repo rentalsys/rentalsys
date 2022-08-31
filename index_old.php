8e5ac0adbcb2d38ab590b7417b81c97f09f907d9
rentalsys <rentalsys@rentalmed.com.br>
2022-08-31 10:59:35 -0300

    atualizar

---
+++ index_old.php
@@ -0,0 +1,20 @@
+<?php
+use app\core\Excecao;
+
+session_start();
+error_reporting(E_ALL & ~E_NOTICE);
+require_once 'app/core/Core.php';
+require_once 'app/helper/helper.php';
+require_once 'config/config.php';
+require_once 'vendor/autoload.php';
+
+try{
+    $core = new Core;
+    $core->run();
+}catch (Error | Exception $e){
+    $erro = new Excecao($e);
+    $erro->mostrar();
+}
+
+
+
